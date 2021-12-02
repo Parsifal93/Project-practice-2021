50fdeb914f3ec132ac6069a5a1d76cb79d44ae85
Vadym Stratiienko <vadimstratiienko@gmail.com>
2021-12-02 19:34:47 +0200

    fix

--- client/src/components/Header/Header.js
+++ client/src/components/Header/Header.js
@@ -94,8 +94,8 @@ class Header extends React.Component {
         </div>
         <div className={styles.loginSignnUpHeaders}>
           <div className={styles.numberContainer}>
-            <img src={`${CONSTANTS.STATIC_IMAGES_PATH}phone.png`} alt="phone" />
-            <span>(877)&nbsp;355-3585</span>
+           <a href="+18773553585"><img src={`${CONSTANTS.STATIC_IMAGES_PATH}phone.png`} alt="phone" />
+            <span>(877)&nbsp;355-3585</span></a>
           </div>
           <div className={styles.userButtonsContainer}>
             {this.renderLoginButtons()}
